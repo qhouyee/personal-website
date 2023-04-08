5198a3ca31811bbdb08bcbf280ce1558c0e71b0d
qhouyee <quekhouyee@gmail.com>
2023-04-08 15:22:52 +0800

    Added contents to Home page and adjusted affected css

---
+++ code/src/const.tsx
@@ -0,0 +1,5 @@
+export const paragraph: string[] = [
+    'I’m a researcher from Singapore. <br> <br> \
+        I am currently working on human-centric interfaces for \
+        real-time digital twin applications for visualising 3D building and asset models.'
+];
--- code/src/routes/Home.tsx
+++ code/src/routes/Home.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { paragraph } from '../const';
+import './scss/home.scss';
+import FigureComponent from '../components/figure/FigureComponent';
+import CharProfile from '../assets/png/charprofile.png';
 
 export interface IHomeProps { }
 
 const Home: React.FC<IHomeProps> = (props: IHomeProps) => {
   return (
     <main>
-      <p>This is the home page</p>
+      <header>
+        <h1 style={{ marginTop: '15%' }}>
+          HELLO, <br />
+          <span style = {{fontSize:'calc(12px + 2vw)'}}><b>Hyde</b> here!</span>
+        </h1>
+        <FigureComponent url={CharProfile} alt={'An illustration of my inner mind'} cssClass="char-profile" />
+      </header>
+      <section>
+        <p dangerouslySetInnerHTML={{ __html: paragraph[0] }}>
+        </p>
+      </section>
     </main>
   );
 };
