7f07649259fbf9f56910238316077ba0d585a7b1
qhouyee <quekhouyee@gmail.com>
2023-04-08 09:33:08 +0800

    Added web application structure based on Vite

---
+++ code/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export interface IAppProps { }
+
+const App: React.FC<IAppProps> = (props: IAppProps) => {
+
+  return (
+    <div>
+      test
+    </div>
+  )
+}
+
+export default App
