2d98bbcb0295d9e02b2e8a57d2b48e8eff0f0ebb
qhouyee <quekhouyee@gmail.com>
2023-05-19 22:54:29 +0800

    dev-animation: Refactor project component into a map

--- code/src/components/content/ProjectSection.tsx
+++ code/src/components/content/ProjectSection.tsx
@@ -4,18 +4,46 @@ import CyclingAnalysis from '../../assets/png/giscycling.png';
 import ProjectComponent from './ProjectComponent';
 import './project.scss';
 
-export interface IProjectProps { }
+interface IProjectProps { }
 
-let portfolioTechStack: string[] = ['HTML', 'CSS', 'Typescript', 'React', 'Vite']
-let cyclingTechStack: string[] = ['HTML', 'CSS', 'Javascript']
+interface Project {
+  projectName: string;
+  projectDescription: string;
+  projectUrl: string;
+  projectFigureUrl: string;
+  projectTech: string[];
+}
+
+let projects: Project[] = [
+  {
+    projectName: '1. PORTFOLIO',
+    projectDescription: '- Designed and developed a portfolio site to experiment with front end technologies and showcase my skills.',
+    projectUrl: 'https://github.com/qhouyee/personal-website',
+    projectFigureUrl: Portfolio,
+    projectTech: ['HTML', 'CSS', 'Typescript', 'React', 'Vite'],
+  },
+  {
+    projectName: '2. CYCLING CULTURE IN XIAMEN',
+    projectDescription: '- Conducted a geospatial analysis of the cycling infrastructure in Xiamen for a university module. \n - Experiment with ArcGIS geospatial web visualization capabilities.',
+    projectUrl: '/webgis',
+    projectFigureUrl: CyclingAnalysis,
+    projectTech: ['HTML', 'CSS', 'Javascript'],
+  },
+];
 
 const ProjectSection: React.FC<IProjectProps> = (props: IProjectProps) => {
   return (
     <>
-      <ProjectComponent projectName='1. PORTFOLIO' projectDescription='- Designed and developed a portfolio site to experiment with front end technologies and showcase my skills.'
-        projectUrl='https://github.com/qhouyee/personal-website' projectFigureUrl={Portfolio} projectTech={portfolioTechStack} />
-      <ProjectComponent projectName='2. CYCLING CULTURE IN XIAMEN' projectDescription={'- Conducted a geospatial analysis of the cycling infrastructure in Xiamen for a university module. \n - Experiment with ArcGIS geospatial web visualization capabilities.'}
-        projectUrl='/webgis' projectFigureUrl={CyclingAnalysis} projectTech={cyclingTechStack} />
+      {projects.map((project, index) => (
+        <ProjectComponent
+          key={index}
+          projectName={project.projectName}
+          projectDescription={project.projectDescription}
+          projectUrl={project.projectUrl}
+          projectFigureUrl={project.projectFigureUrl}
+          projectTech={project.projectTech}
+        />
+      ))}
     </>
   );
 }
